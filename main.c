998f301eed225265d4d9669e586356183a4bb983
Nahuel Rizzo <52260647+rizzo2208@users.noreply.github.com>
2022-04-12 20:39:58 -0300

    comentarios
    se agregan comentarios.

--- main.c
+++ main.c
@@ -4,22 +4,30 @@
 
 int main()
 {
+    //definicion e inicializacion de variables.
+    
     char contrasenia [20];
     char prueba[20];
     int i=0;
     int comp=0;
-
+    
+    //se solicita la contraseña al usuario.
+    
     printf("ingrese la contrasenia:");
     gets(contrasenia);
-
+    
+    //se le solicita la contraseña 3 veces comparando la variable pueba y contraseña.
+    
     for(i=0;i<3;i++){
         printf("ingrese nuevamente la contrasenia:");
         gets(prueba);
         comp= strcmp(&contrasenia, &prueba);
-
+        
+    //se verifica si hubo coincidencia y se muestra por teclado los respectivos mensajes.
+        
         if(comp==0){
             system("cls");
-            printf("Felicitaciones,record�s tu contrasenia\n");
+            printf("Felicitaciones,recordás tu contrasenia\n");
             i=3;
         }
     }
